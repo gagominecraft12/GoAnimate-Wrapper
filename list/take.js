aa4708028d9d8974fdd71f8a19ddc40ace8bceaf
GagoAnimate <65516039+gagominecraft12@users.noreply.github.com>
2020-09-21 13:21:42 -0500

    Create take.js

---
+++ list/take.js
@@ -0,0 +1,8 @@
+js FileList with toString as a tag with JSON cc_char WritableStream with Request File
+request/get.js
+as getMatchedCSSRules
+with cc_char on Node as NodeFilter And Node.js
+MSMediaKeyNeededEvent AbortSignal
+
+
+                                 github repo as visual studio 
